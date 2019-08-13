d974dfc077a3939452c960a2a2f7d8d0aebf1d2d
이강호 <zxdcf170@naver.com>
2019-08-13 17:48:47 +0900

    초기 커밋.

---
+++ PersonalInformationFinder/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonalInformationFinder
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
